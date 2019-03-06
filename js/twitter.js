a034aa720999cf0704f4911316e8bbf478f40a81
Sam Liu <ontouchstart@gmail.com>
2019-03-06 10:29:43 -0500

    data-size: large

--- js/twitter.js
+++ js/twitter.js
@@ -1,12 +1,13 @@
 const twitter = (container) => {
   const button = document.createElement('a');
-  button.innertText = 'Twitter';
+  button.innertText = 'Tweet';
   button.setAttribute('class', 'twitter-share-button');
+  button.setAttribute('data-size', 'large');
   button.setAttribute('data-hashtags', 'mocaspike150');
   button.setAttribute('data-show-count', true);
 
   const js = document.createElement('script');
-  js.src = 'https://platform.twitter.com/widgets.js'
+  js.src = 'https://platform.twitter.com/widgets.js';
   js.async = true;
 
   container.appendChild(button);
